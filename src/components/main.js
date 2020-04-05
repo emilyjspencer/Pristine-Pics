189621777fbf80e3cf6babef4685690142dcbf46
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-04-05 19:25:23 +0100

    Initializes create-react-app. Creates first files. Pristine Pics is rendered in the browser

---
+++ src/components/main.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import Title from './title'
+
+
+class Main extends Component {
+  render() {
+      return <div>
+          <Title title={'Pristine Pics'}/>
+      </div>
+  }
+}
+
+
+export default Main
