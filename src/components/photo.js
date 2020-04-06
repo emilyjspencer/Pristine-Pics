ba388a018963cd5fe0525fd1ef7edd5d6592d0e1
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-04-06 15:52:42 +0100

    Gives photo and PristinePics components styling, using Flexbox properties

--- src/components/photo.js
+++ src/components/photo.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Photo extends Component {
     render() {
       const post = this.props.post
-      return <figure></figure>
+      return <figure className="captionAndPic"></figure>
     }
 }
 
--- src/components/pristinePics.js
+++ src/components/pristinePics.js
@@ -3,7 +3,7 @@ import Photo from './photo'
 
 class PristinePics extends Component {
     render() {
-       return <div>
+       return <div className="photoPage">
          {this.props.posts.map((post, index) => <Photo key={index}post={post} />)}
         </div>
     } 
