7d3644a17fab57f19e91701a5f77688ff56392af
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-04-06 15:59:11 +0100

    Renders photos on the page

--- src/components/photo.js
+++ src/components/photo.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 class Photo extends Component {
     render() {
       const post = this.props.post
-      return <figure className="captionAndPic"></figure>
+      return <figure className="captionAndPic">
+        <img src={post.imageLink} alt={post.description}></img>
+      </figure>
     }
 }
 
