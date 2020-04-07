c4fc2c99d6fc94d1a56b85e84028276d0a39b1e6
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-04-07 22:44:50 +0100

    Imports Link from React Router. Uses a <Link> componen to navigate between pages. No need for #

--- src/components/pristinePics.js
+++ src/components/pristinePics.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import Photo from './photo'
+import {Link} from 'react-router-dom'
 
 class PristinePics extends Component {
     render() {
     
        return <div>
-           <a className="add-photo-link" onClick={this.props.onChangePage} href="#AddPhoto"></a>
+           <Link className="add-photo-link" to="/AddPhoto"> </Link>
         
           <div className="photoPage">
          {this.props.posts.map((post, index) => <Photo key={index}post={post} onDeletePhoto={this.props.onDeletePhoto} />)}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styling/stylesheet.css';
-import Main from './components/main.js'
+import Main from './components/main'
 import {BrowserRouter} from 'react-router-dom'
 
 
 
 
-ReactDOM.render(<BrowserRouter>=Main/></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><Main/></BrowserRouter>, document.getElementById('root'));
 
 
