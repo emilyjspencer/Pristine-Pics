cf3f10317987c5f6dfbf6fd522fd5950da027e2d
emilyjspencer <emilyspencer89@hotmail.co.uk>
2023-05-09 15:14:44 +0100

    Formats pages

--- src/components/main.js
+++ src/components/main.js
@@ -1,79 +1,84 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import Title from './title'
-import PristinePics from './pristinePics'
-import UploadPhoto from './uploadPhoto'
-import { Route } from 'react-router-dom'
-
-
-
-
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Title from "./title";
+import PristinePics from "./pristinePics";
+import UploadPhoto from "./uploadPhoto";
+import { Route } from "react-router-dom";
 
 class Main extends Component {
-    constructor() {
-        super()
-        this.state = {
-            posts: [{
-                id: "0",
-                description: "Beach shot",
-                imageLink: "https://aspenchamber.org/sites/default/files/images/Beach.stockimage.jpg"
-                }, {
-                id: "1",
-                description: "Mountain shot",
-                imageLink: "https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/Taiwan_16x9.jpg?itok=-5n1YhQH"
-                }, {
-                id: "2",
-                description: "Forest shot",
-                imageLink: "http://allpicts.in/wp-content/uploads/2018/09/High-Resolution-Nature-Photo-with-Picture-of-Autumn-Forest.jpg"
-                }],
+  constructor() {
+    super();
+    this.state = {
+      posts: [
+        {
+          id: "0",
+          description: "Beach shot",
+          imageLink:
+            "https://aspenchamber.org/sites/default/files/images/Beach.stockimage.jpg",
+        },
+        {
+          id: "1",
+          description: "Mountain shot",
+          imageLink:
+            "https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/Taiwan_16x9.jpg?itok=-5n1YhQH",
+        },
+        {
+          id: "2",
+          description: "Forest shot",
+          imageLink:
+            "http://allpicts.in/wp-content/uploads/2018/09/High-Resolution-Nature-Photo-with-Picture-of-Autumn-Forest.jpg",
+        },
+      ],
+      page: "photos",
+    };
+    this.deletePhoto = this.deletePhoto.bind(this);
+  }
 
-                page: 'photos'
-        }
-        this.deletePhoto = this.deletePhoto.bind(this);
-    }
+  addPhoto(postSubmitted) {
+    this.setState((state) => ({
+      posts: state.posts.concat([postSubmitted]),
+    }));
+  }
 
-    addPhoto(postSubmitted) {
-        this.setState(state => ({
-            posts: state.posts.concat([postSubmitted])
-        }))
-    }
-    
-        
-    deletePhoto(photoDeleted) {
-        console.log(photoDeleted.description)
-        this.setState((state) => ({
-            posts: state.posts.filter(photo => photo !== photoDeleted)
-        })) 
-    }
-
-
+  deletePhoto(photoDeleted) {
+    console.log(photoDeleted.description);
+    this.setState((state) => ({
+      posts: state.posts.filter((photo) => photo !== photoDeleted),
+    }));
+  }
 
   render() {
-      return (
-     
+    return (
       <div>
-        <Route exact path = "/" render={() => (
-          <div>
-            <Title title={'Pristine Pics'}/>
-            <PristinePics posts={this.state.posts} onDeletePhoto={this.deletePhoto} onChangePage = {this.changePage}/>
-          </div>
-        )}/>
-        
-          <Route path="/AddPhoto" render ={({history}) => (
-          
-              <UploadPhoto onAddPhoto={(addedPost) => {
-                 this.addPhoto(addedPost)
-                 history.push('/')
-               
-              }} />
-          )}/>
-            
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <div>
+              <Title title={"Pristine Pics"} />
+              <PristinePics
+                posts={this.state.posts}
+                onDeletePhoto={this.deletePhoto}
+                onChangePage={this.changePage}
+              />
             </div>
-       
-         
-      )
-    }
+          )}
+        />
+
+        <Route
+          path="/AddPhoto"
+          render={({ history }) => (
+            <UploadPhoto
+              onAddPhoto={(addedPost) => {
+                this.addPhoto(addedPost);
+                history.push("/");
+              }}
+            />
+          )}
+        />
+      </div>
+    );
+  }
 }
 
-
-export default Main
+export default Main;
--- src/components/photo.js
+++ src/components/photo.js
@@ -1,19 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class Photo extends Component {
-    render() {
-      const post = this.props.post
-      return <figure className="captionAndPic">
+  render() {
+    const post = this.props.post;
+    return (
+      <figure className="captionAndPic">
         <img className="pic" src={post.imageLink} alt={post.description} />
-        <figcaption> <p> {post.description} </p> </figcaption>
+        <figcaption>
+          {" "}
+          <p> {post.description} </p>{" "}
+        </figcaption>
         <div className="container-button">
-          <button onClick = {() => {
-            this.props.onDeletePhoto(post)
-          }}>Delete</button>
+          <button
+            onClick={() => {
+              this.props.onDeletePhoto(post);
+            }}
+          >
+            Delete
+          </button>
         </div>
       </figure>
-    }
+    );
+  }
 }
 
- 
-export default Photo
+export default Photo;
