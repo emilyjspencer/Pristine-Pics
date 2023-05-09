cd7e7cbd7a5b266afb123ebca01c99f3d58e3bb1
emilyjspencer <emilyspencer89@hotmail.co.uk>
2023-05-09 15:36:56 +0100

    Fixes issue with cross link not working. Can now add photos

--- src/components/pristinePics.js
+++ src/components/pristinePics.js
@@ -1,19 +1,27 @@
-import React, {Component} from 'react';
-import Photo from './photo'
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import Photo from "./photo";
+import { Link } from "react-router-dom";
 
 class PristinePics extends Component {
-    render() {
-    
-       return <div>
-           <Link className="add-photo-link" to="/AddPhoto"> </Link>
-        
-          <div className="photoPage">
-         {this.props.posts.map((post, index) => <Photo key={index}post={post} onDeletePhoto={this.props.onDeletePhoto} />)}
-          </div>
+  render() {
+    return (
+      <div>
+        <Link className="add-photo-link" to="/AddPhoto">
+          {" "}
+        </Link>
+
+        <div className="photoPage">
+          {this.props.posts.map((post, index) => (
+            <Photo
+              key={index}
+              post={post}
+              onDeletePhoto={this.props.onDeletePhoto}
+            />
+          ))}
         </div>
-  
-    } 
+      </div>
+    );
+  }
 }
 
-export default PristinePics
+export default PristinePics;
